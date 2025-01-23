a665181950405a16b0158fe79d525ea5177a8a70
KamalAbdullayev0 <abdullayevkamal16.08@gmail.com>
2025-01-24 00:57:51 +0400

    Initial Commit

---
+++ Wp_chat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wp_chat
+//
+//  Created by Kamal Abdullayev on 24.01.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
